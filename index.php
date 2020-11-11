bc60ef80461f9dfac5d192de04309889098e7efe
root <root@localhost.localdomain>
2020-11-11 08:29:09 +0300

    include a .htaccess file

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
   require_once('include/connect.php');
+
  ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php include('include/static.php');?>
+  <?php require_once('include/static.php');?>
 </head>
 <body>
   <!-- Start your project here-->
   <!--Navbar-->
-<nav class="navbar navbar-expand-lg navbar-light">
+  <header>
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
   <!-- Navbar brand -->
-    <a class="navbar-brand" href="#" style="color:black;">PachekoMall.</a>
+  <div class="container">
+    <a class="navbar-brand" href="#" style="color:black;">Chrono360.</a>
       <!-- Collapse button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
     aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +29,7 @@
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">CATEGORY</a>
         <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Jewllery</a>
+          <a class="dropdown-item" href="#">Jewellery</a>
           <a class="dropdown-item" href="#">Watch</a>
       </div>
       </li>
@@ -40,7 +43,7 @@
       </div>
       </li>
       <li class='nav-item'>
-        <a class="nav-link" href="#">OPEN STORE</a>
+        <a class="nav-link" href="store/new">OPEN STORE</a>
       </li>
     </ul>
     <!-- Links -->
@@ -50,17 +53,17 @@
           <li class="nav-item">
             <a class="nav-link" href="#">POLICY</a>
           </li>
-    <li class="nav-item">
+    <!-- <li class="nav-item">
       <a class="nav-link waves-effect waves-light">
         <i class="fas fa-hand-holding-usd"></i>
       </a>
-    </li>
+    </li> -->
 
       </div>
     </form>
   </div>
   <!-- Collapsible content -->
-
+</div>
 </nav>
 <!--/.Navbar-->
 <!-- Full Page Intro -->
@@ -76,19 +79,17 @@
                <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-2 wow fadeInDown" data-wow-delay="0.3s"><strong>Web Up your business</strong></h1>
                <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
                <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Chronographs & Jewelleries</strong></h5>
-               <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">Open Your store</a>
+               <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s" href="store/new">Open Your store</a>
 
-               <div class="container">
-                 <div class="row">
+
+               <!-- Implementing the search functionality -->
                <div class="input-group col-md-4 offset-md-4  center-block">
                  <div class="input-group-prepend">
-                   <span class="input-group-text" id="validationTooltipUsernamePrepend glyphicon glyphicon-search">@</span>
+                   <span class="input-group-text" id="validationTooltipUsernamePrepend" name="search_btn"><i class="fas fa-search"></i></span>
                  </div>
-                 <br><input type="text" class="form-control" id="validationTooltipUsername" placeholder="Search product" aria-describedby="validationTooltipUsernamePrepend" required>
+                 <br><input type="text" name="search_field"class="form-control" id="validationTooltipUsername" placeholder="Search product" aria-describedby="validationTooltipUsernamePrepend" required>
                </div>
              </div>
-           </div>
-           </div>
 
                <!-- <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">About me</a> -->
              </div>
@@ -99,21 +100,24 @@
          <!-- Content -->
        </div>
        <!-- Mask & flexbox options-->
-
-
+       </header>
 <div class="container">
   <div class="row">
     <div class="col-sm-4 col-md-4 d-md-none">
-      <span class="badge badge-pill ">Rolex</span>
-      <span class="badge badge-pill ">Quartz</span>
-      <span class="badge badge-pill ">Chrono</span>
-      <span class="badge badge-pill ">Chrono</span>
-
+      <span class="badge ">Rolex</span>
+      <span class="badge ">Quartz</span>
+      <span class="badge ">Chrono</span>
+      <span class="badge ">Chrono</span>
+      <span class="badge ">Rolex</span>
+      <span class="badge ">Quartz</span>
+      <span class="badge ">Chrono</span>
+      <span class="badge ">Chrono</span>
     </div>
   </div>
 </div>
 <div class="container">
-<br><h4>New In Stock</h4>
+<br>
+<h4 class="font-weight-bold">New In Stock</h4>
 </div>
 <?php
       echo '<div class="container">';
@@ -130,6 +134,7 @@
                   // <!-- Card content -->
             echo "<div class='card-body card-body-cascade'>";
             echo '<h5 class="pink-text pb-2 pt-1"></i>'.$row['title'].'</h5>';
+            // echo'<h4 class="font-weight-bold card-title"><strike>Ksh. '.$row['list_price'].'</strike></h4>';
             echo'<h4 class="font-weight-bold card-title">Ksh. '.$row['price'].'</h4>';
             echo'<p class="card-text">'.$row['description'].'</p>';
             echo'<a class="btn btn-primary">Order Now</a>';
@@ -142,6 +147,8 @@
 
        }
     ?>
+
+
   <!-- jQuery -->
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
@@ -150,6 +157,7 @@
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
   <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="js/app.js"></script>
   <!-- Your custom scripts (optional) -->
   <script type="text/javascript">// Animations init
     new WOW().init();
---
+++ store/new.php
@@ -0,0 +1,6 @@
+<?php
+  require('../include/connect.php');
+  include_once('../include/static.php');
+
+ ?>
+<h1>Hello</h1>
